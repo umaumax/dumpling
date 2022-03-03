493985a6db8d68c56c487d54f9be35882ecb2c9c
umaumax <umauma7083@gmail.com>
2022-03-03 22:28:52 +0900

    Add algos
    sha1, sha256, md5

---
+++ src/lib.rs
@@ -0,0 +1,90 @@
+#[macro_export]
+macro_rules! dump_hex {
+    ($x:expr) => {
+        $x.to_be_bytes()
+            .iter()
+            .map(|n| format!("{:02x}", n))
+            .collect::<String>()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_HEX {
+    ($x:expr) => {
+        $x.to_be_bytes()
+            .iter()
+            .map(|n| format!("{:02X}", n))
+            .collect::<String>()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_iter_hex {
+    ($x:expr) => {
+        $x.iter()
+            .enumerate()
+            .map(|(i, n)| format!("{:02x}{}", n, if i % 8 == 7 { " " } else { "" }))
+            .collect::<String>()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_iter_HEX {
+    ($x:expr) => {
+        $x.iter()
+            .enumerate()
+            .map(|(i, n)| format!("{:02X}{}", n, if i % 8 == 7 { " " } else { "" }))
+            .collect::<String>()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_sha1 {
+    ($x:expr) => {
+        sha1_smol::Sha1::from($x).digest().to_string()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_SHA1 {
+    ($x:expr) => {
+        sha1_smol::Sha1::from($x)
+            .digest()
+            .to_string()
+            .to_uppercase()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_sha256 {
+    ($x:expr) => {
+        hmac_sha256::Hash::hash($x)
+            .iter()
+            .map(|n| format!("{:02x}", n))
+            .collect::<String>()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_SHA256 {
+    ($x:expr) => {
+        hmac_sha256::Hash::hash($x)
+            .iter()
+            .map(|n| format!("{:02X}", n))
+            .collect::<String>()
+    };
+}
+
+#[macro_export]
+macro_rules! dump_md5 {
+    ($x:expr) => {
+        format!("{:x}", md5::compute($x))
+    };
+}
+
+#[macro_export]
+macro_rules! dump_MD5 {
+    ($x:expr) => {
+        format!("{:X}", md5::compute($x))
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use dumpling::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    {
+        println!("# lower case macro!");
+        let v = 1.23_f32;
+        println!("{} -> {}", v, dump_hex!(v));
+        let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        println!("{:?} -> {}", v, dump_iter_hex!(v));
+        let v = "Hello world!";
+        println!("{:?} -> {}", v, dump_iter_hex!(v.as_bytes()));
+        let v = "Hello world!";
+        println!("sha1: {:?} -> {}", v, dump_sha1!(v.as_bytes()));
+        let v = "Hello world!";
+        println!("sha256: {:?} -> {}", v, dump_sha256!(v.as_bytes()));
+        let v = "Hello world!";
+        println!("md5: {:?} -> {}", v, dump_md5!(v.as_bytes()));
+    }
+    println!();
+    {
+        println!("# upper case macro!");
+        let v = 1.23_f32;
+        println!("{} -> {}", v, dump_HEX!(v));
+        let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        println!("{:?} -> {}", v, dump_iter_HEX!(v));
+        let v = "Hello world!";
+        println!("{:?} -> {}", v, dump_iter_HEX!(v.as_bytes()));
+        let v = "Hello world!";
+        println!("sha1: {:?} -> {}", v, dump_SHA1!(v.as_bytes()));
+        let v = "Hello world!";
+        println!("sha256: {:?} -> {}", v, dump_SHA256!(v.as_bytes()));
+        let v = "Hello world!";
+        println!("md5: {:?} -> {}", v, dump_MD5!(v.as_bytes()));
+    }
+
+    Ok(())
+}
